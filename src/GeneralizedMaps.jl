7830505d725e689b6be3f61246e40cfc5104fba0
Derek Thomas <derekcthomas@gmail.com>
2015-04-18 17:26:03 -0500

    Removed Nullable alphas, changed polygon to polygon! operator on GeneralizedMap.

--- src/GeneralizedMaps.jl
+++ src/GeneralizedMaps.jl
@@ -1,13 +1,15 @@
 module GeneralizedMaps
 using Docile
+
+
 type Dart{T, S}
     index::T
-    # involutions or pointers to other darts in the GeneralizedMap
-    alpha::Vector{Nullable{Dart{T,S}}}
+    # involutions or references to other darts in the GeneralizedMap
+    alpha::Vector{Dart{T,S}}
     # data associated with a global set of darts (an orbit or k-cell)
     globalembed::Vector{Nullable{S}}
-    # boolean to indicate if this is the storage node for a global set of data (this may be unecessary)
-    iskey::Vector{Bool}
+    # references to the dart where the data is stored for each dim
+    embedloc::Vector{Nullable{Dart{T,S}}}
     # marker for traversal algorithms
     ismarked::Bool
 end
@@ -17,62 +19,82 @@ function Base.print{T, S}(d::Dart{T,S})
     println("Index: ", d.index)
     print("alphas: ")
     for a in d.alpha
-        if !isnull(a)
-            print(get(a).index, " ")
-        end
+        print(a.index, " ")
     end
+    println()
 end
 
 function Dart(dim, T, S)
-    alphas = fill(Nullable{Dart{T,S}}(), dim + 1)
-    globalembed = fill(Nullable{S}(), dim + 1)
-    iskey = [false for i in range(1, dim + 1)]
-    return Dart{T, S}(0, alphas, globalembed, iskey, false)
+    d = Dart{T, S}(zero(T), Dart{T,S}[], Nullable{S}[], Nullable{Dart{T,S}}[], false)
+    d.alpha = fill(d, dim + 1)
+    d.globalembed = fill(Nullable{S}(), dim + 1)
+    d.embedloc = fill(Nullable{Dart{T,S}}(), dim + 1)
+    return d
 end
-
-Dart(T, S) = Dart(0, T, S)
 
 function dim{T, S}(d::Dart{T, S})
     return length(d.alpha) - 1
 end
 
 Docile.@doc """
+Initialize an empty dart
+""" ->
+Dart(T, S) = Dart(0, T, S)
+
+Docile.@doc """
 A collection of connected darts.
 """ ->
-type GeneralizedMap{T,S}
-    darts::Vector{Dart{T,S}}
+type GeneralizedMap{I,T,S}
+    darts::Dict{I, Dart{T,S}}
 end
 
-GeneralizedMap(T::Type, S::Type) = GeneralizedMap(Dart{T, S}[])
+Docile.@doc """
+Initialize an empty GeneralizedMap with dart dimension of type T and storage type S.  The indices of the GeneralizedMap are set as Int64.
+""" ->
+GeneralizedMap(T::Type, S::Type) = GeneralizedMap(Dict{Int64, Dart{T, S}}())
 
-function Base.push!{T, S}(gmap::GeneralizedMap{T,S}, dart::Dart{T})
-    dart.index = convert(T, length(gmap.darts) + 1)
-    push!(gmap.darts, dart)
+Docile.@doc """
+Push dart onto the GeneralizedMap gmap.  The index is updated and the result is stored.
+""" ->
+function Base.push!{I, T, S}(gmap::GeneralizedMap{I,T,S}, dart::Dart{T,S})
+    dart.index = convert(I, length(gmap.darts) + 1)
+    gmap.darts[dart.index] = dart
 end
 
 
+Docile.@doc """
+Get the alpha reference for the dart d at dimension i
+""" ->
 function alpha{T,S}(d::Dart{T, S}, i::T)
-    return get(d.alpha[i+1])
+    return d.alpha[i+1]
 end
 
-function setalpha{T,S}(d::Dart{T, S}, i, alpha::Dart{T,S})
-    d.alpha[i+1] = Nullable(alpha)
+Docile.@doc """
+Set the alpha reference for the dart d at dimension i to the dart other
+""" ->
+function setalpha!{T,S}(d::Dart{T, S}, i, other::Dart{T,S})
+    d.alpha[i+1] = other
 end
 
+Docile.@doc """
+Get the embedded data for dimension i of this dart.
+""" ->
 function data{T, S}(d::Dart{T, S}, i)
     return get(d.globalembed[i+1])
 end
 
-function setdata{T, S}(d::Dart{T, S}, i, data::S)
-    d.globalembed[i+1] = Nullable(data)
+Docile.@doc """
+Set the reference at which the data for dimension i of this dart is stored.
+""" ->
+function setdataloc!{T, S}(d::Dart{T, S}, i, loc::Dart{T,S})
+    d.embedloc[i+1] = Nullable(loc)
 end
 
-function setdata{T, S}(d::Dart{T, S}, i, data::Nullable{S})
+Docile.@doc """
+set the embedded data for dimension i in the dart d to data.
+""" ->
+function setdata!{T, S}(d::Dart{T, S}, i, data::Nullable{S})
     d.globalembed[i+1] = data
-end
-
-function isnullalpha(d::Dart, i)
-    return isnull(d.alpha[i+1])
 end
 
 Docile.@doc """
@@ -122,12 +144,10 @@ function traverse{T,S}(orbit::Orbit{T}, start::Dart{T,S}, f::Function)
         d = pop!(stack)
         f(d)
         for j in orbit.index
-            if !isnullalpha(d, j)
-                next = alpha(d, j)
-                if !next.ismarked
-                    mark!(next)
-                    push!(stack, next)
-                end
+            next = alpha(d, j)
+            if !next.ismarked
+                mark!(next)
+                push!(stack, next)
             end
         end
     end
@@ -147,31 +167,26 @@ function collectcelldarts{T,S}(d::Dart{T,S}, k )
 end
 
 function findcellkey{T,S}(start::Dart{T,S}, dim)
-    for d in collectcelldarts(start, dim)
-        if d.iskey[dim+1]
-            return d
-        end
-    end
-    return Dart(T, S)
+    return start.embedloc[dim+1]
 end
 
-function dispatchembedding{T,S}(start::Dart{T,S}, dim, data)
+function dispatchembedding{T,S}(start::Dart{T,S}, dim, loc::Dart{T,S})
     for d in collectcelldarts(start, dim)
-        setdata(d, dim, data)
+        setdataloc!(d, dim, start)
     end
 end
     
 function sharecopyembedding{T,S}(d1::Dart{T,S}, d2::Dart{T,S}, dim)
     k1 = findcellkey(d1, dim)
     k2 = findcellkey(d2, dim)
-    if !isequal(k1, Dart{T,S}())
-        new_em = d2.globalembed[dim+1]
-        d1.iskey[dim+1] = true
-        dispatchembedding(d1, dim, new_em)
-    else
-        new_em = d1.globalembed[dim+1]
-        d2.iskey[dim+1] = true
-        dispatchembedding(d2, dim, new_em)
+    if !isequal(k1, k2)
+        if isnull(k1)
+            new_em = d2.globalembed[dim+1]
+            setdata!(d1, dim, new_em)
+            dispatchembedding(d1, dim, d1)
+        else
+            dispatchembedding(d1, dim, d1)
+        end
     end
 end
 
@@ -184,13 +199,40 @@ function sew!{T, S}(d1::Dart{T, S}, d2::Dart{T, S}, dim)
             k1 = findcellkey(dp1, i)
             k2 = findcellkey(dp2, i)
             if !isequal(k1, k2)
-                k2.iskey[i+1] = false
+                k2.embedloc[i+1] = k1
                 dispatchembedding(k2, i, k1.globalembed[i+1])
             end
         end
-        dp1.alpha[dim+1] = dp2
-        dp2.alpha[dim+1] = dp1
+        setalpha!(dp1, dim, dp2)
+        setalpha!(dp2, dim, dp1)
     end
 end
 
+function polygon!(g, dim, n)
+    for i in range(1, 2 * n)
+        T = typeof(g)
+        push!(g, Dart(dim, T.parameters[2], T.parameters[3]))
+    end
+    # first sew pairs of darts to each other to form vertices
+    for i in range(1, 2, div(length(g.darts), 2))
+        sew!(g.darts[i], g.darts[i+1], 0)
+    end
+    # now create edges
+    for i in range(2, 2, div(length(g.darts), 2))
+        sew!(g.darts[i], g.darts[(i+1)%(2*n)], 1)
+    end
+end
+
+function collectkcells(g::GeneralizedMap, k)
+    cells = Set{Set{eltype(g.darts)[2]}}()
+    for (i, d) in g.darts
+        push!(cells, collectcelldarts(d, k))
+    end
+    return cells
+end
+
+function countkcells(g::GeneralizedMap, k)
+    cells = collectkcells(g, k)
+    return length(cells)
+end
 end # module
